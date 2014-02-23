16d7dbfd6783f21beda13a834889bac7963f2c57
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-02-23 11:30:15 -0500

    Re structuring

--- lib/encrypter_decrypter_files.rb
+++ lib/encrypter_decrypter_files.rb
@@ -1,7 +1,7 @@
 require 'openssl'
 require 'yaml'
 require 'awesome_print'
-require 'helper'
+require 'encrypter_decrypter/helper.rb'
 require 'encrypter_decrypter/base.rb'
 require 'encrypter_decrypter/decrypt.rb'
 require 'encrypter_decrypter/encrypt.rb'
